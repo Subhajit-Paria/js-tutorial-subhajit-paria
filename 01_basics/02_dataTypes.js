a60afefa53149491c812a37ea5969bcd873e3fee
Subhajit-Paria <subhajitrsgis@gmail.com>
2026-01-03 12:33:00 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3)  // code readability should be high
+
+console.log("Subhajit");
+
+let name = "Subhajit" //string
+let age = 18  // number
+let isLoggedIn = false // boolean
+let state;
+
+
+//Premetive Data Type//
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => when value not assign
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); // object
+
+
+// https://tc39.es/ecma262/#prod-VariableStatement   // javascript document
